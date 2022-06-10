b4ff5fef96edc1d9638507eb80a001b5a93aa6d2
Aman Agarwal <aman09agarwal@gmail.com>
2022-06-10 14:12:17 +0530

    Updated Token time to 30 min
    Co-authored-by: Akash Soni <akashsoni1102@gmail.com>

--- src/main/java/com/tweetapp/service/JwtUtil.java
+++ src/main/java/com/tweetapp/service/JwtUtil.java
@@ -40,7 +40,7 @@ public class JwtUtil {
     private String createToken(Map<String, Object> claims, String subject) {
 
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 5))// token for 5 min
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))// token for 30 min
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 
